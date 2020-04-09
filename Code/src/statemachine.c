757b79958735ca3ec8a068406565876e9974e303
knurtz <34451617+knurtz@users.noreply.github.com>
2020-04-09 09:41:34 +0200

    [Code] Added function to compare systime

--- Code/src/statemachine.c
+++ Code/src/statemachine.c
@@ -6,7 +6,7 @@
 
 
 
-
+static systime_t start_time;            // used to store systime when lever gets pulled down
 
 ew_state_t handle_event(eventid_t new_event, ew_state_t current_state) {
 
@@ -19,15 +19,15 @@ ew_state_t handle_event(eventid_t new_event, ew_state_t current_state) {
         case EW_LEVER_DOWN_EVENT:
             switch (current_state) {
                 case EW_ALARM_RINGING:
-                    //stopRingtone();
-                    //save timestamp for later
+                    stopRingtone();
+                    start_time = chVTGetSystemTimeX();
                     return(current_state);
                 case EW_SHOW_ALARM:
-                    //toggleAlarmEnable(toggle_value);
+                    toggleAlarmEnable(toggle_value);
                     return(current_state);
                 case EW_SET_HOURS:
                 case EW_SET_MINUTES:
-                    //if (toggle_status == 0) toggleDST();
+                    if (toggle_value == 0) toggleDST();
                     return(current_state);
             }
             return(current_state);
@@ -37,18 +37,17 @@ ew_state_t handle_event(eventid_t new_event, ew_state_t current_state) {
                 case EW_IDLE:
                     return(enterStandby());
                 case EW_ALARM_RINGING:
-                    //get time, compare to saved value from line 16
-                    //if elapsed time < 2s:
-                    //setAlarm(alarm_id, current_time + snooze_time);
-                    //else
-                    //setAlarm(alarm_id, stored_alarm_time)
+                    if(chVTIsSystemTimeWithinX(start_time, start_time + TIME_MS2I(500)))
+                        setAlarm(/*alarm_id, current_time + snooze_time*/);
+                    else
+                        setAlarm(/*alarm_id, stored_alarm_time*/);
                     return(enterIdle(EW_TIMEOUT_SHORT));
             }
             return(current_state);
 
         case EW_TOGGLE_CHANGE_EVENT:
             if (current_state != EW_ALARM_RINGING) {
-                if (toggle_value == 0) return(enterIdle());
+                if (toggle_value == 0) return(enterIdle(EW_TIMEOUT_LONG));
                 return(enterShowAlarm(toggle_value));
             }
             return(current_state);
@@ -61,8 +60,8 @@ ew_state_t handle_event(eventid_t new_event, ew_state_t current_state) {
                 case EW_SET_HOURS:
                     return(enterSetMinutes(toggle_value));
                 case EW_SET_MINUTES:
-                    //saveTime(toggle_value);
-                    //if (toggle_value == 0) return(enterIdle());
+                    saveTime(toggle_value);
+                    if (toggle_value == 0) return(enterIdle(EW_TIMEOUT_LONG));
                     return(enterShowAlarm(toggle_value));
             }
             return(current_state);
@@ -80,10 +79,10 @@ ew_state_t handle_event(eventid_t new_event, ew_state_t current_state) {
             return(current_state);
 
         case EW_SNOOZE_TIMER_EVENT:
-            //stopRingtone();
-            //toggleAlarmEnable(currently_ringing);
-            //saveAlarm(currently_ringing);
-            return(enterIdle());
+            stopRingtone();
+            toggleAlarmEnable(/*currently_ringing*/);
+            saveAlarm(/*currently_ringing*/);
+            return(enterIdle(EW_TIMEOUT_SHORT));
 
         case EW_STANDBY_TIMER_EVENT:
             return(enterStandby());
