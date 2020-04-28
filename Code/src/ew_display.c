9425ae12de3900c3bae6fd17ce26d41779beca61
Johannes Herold <johannes_herold@turag.de>
2020-04-28 21:37:28 +0200

    [Code] Some helper functions to display data.

--- Code/src/ew_display.c
+++ Code/src/ew_display.c
@@ -20,6 +20,12 @@ to generate a PWM signal for the display's filament and communicating with the P
 #define PT6312_WRITE_DISPLAY_DATA   0
 #define PT6312_AUTO_INCREMENT       0
 
+// options for command 3
+#define PT6312_ADDRESS_DIGIT0       0
+#define PT6312_ADDRESS_DIGIT1       2
+#define PT6312_ADDRESS_DIGIT2       4
+#define PT6312_ADDRESS_DIGIT3       6
+
 // options for command 4
 #define PT6312_PULSEWIDTH_MIN       0
 #define PT6312_PULSEWIDTH_1         0
@@ -38,22 +44,18 @@ to generate a PWM signal for the display's filament and communicating with the P
 typedef union
 {
     struct {
-        uint8_t seg_1 : 1;
-        uint8_t seg_g : 1;
-        uint8_t seg_f : 1;
-        uint8_t seg_e : 1;
-        uint8_t seg_d : 1;
-        uint8_t seg_c : 1;
-        uint8_t seg_b : 1;
         uint8_t seg_a : 1;
-        uint8_t seg16 : 1;
-        uint8_t seg15 : 1;
-        uint8_t seg14 : 1;
-        uint8_t seg13 : 1;
-        uint8_t seg_point : 1;
-        uint8_t seg_4 : 1;
-        uint8_t seg_3 : 1;
+        uint8_t seg_b : 1;
+        uint8_t seg_c : 1;
+        uint8_t seg_d : 1;
+        uint8_t seg_e : 1;
+        uint8_t seg_f : 1;
+        uint8_t seg_g : 1;
+        uint8_t seg_1 : 1;
         uint8_t seg_2 : 1;
+        uint8_t seg_3 : 1;
+        uint8_t seg_4 : 1;
+        uint8_t seg_p : 1;
     } segments;
     struct {
         uint8_t low_byte;
@@ -62,8 +64,94 @@ typedef union
     uint16_t complete;
 } DigitData;
 
-DigitData digits[4];
+static DigitData digits[4];
 
+
+//===========================================================================
+// Modify display content
+//===========================================================================
+
+// value can be a number between 0 - 9 for now
+static void setDigit(uint8_t n, char value) {
+    if (n > 3) return;
+
+    digits[n].complete |= 0x7f;        // initially turn on all segments needed for numbers
+
+    switch (value) {
+        case 0:
+            digits[n].segments.seg_g = 0;
+            break;
+        case 1:
+            digits[n].segments.seg_a = 0;
+            digits[n].segments.seg_d = 0;
+            digits[n].segments.seg_e = 0;
+            digits[n].segments.seg_f = 0;
+            digits[n].segments.seg_g = 0;
+            break;
+        case 2:
+            digits[n].segments.seg_c = 0;
+            digits[n].segments.seg_f = 0;
+            break;
+        case 3:
+            digits[n].segments.seg_e = 0;
+            digits[n].segments.seg_f = 0;
+            break;
+        case 4:
+            digits[n].segments.seg_a = 0;
+            digits[n].segments.seg_d = 0;
+            digits[n].segments.seg_e = 0;
+            break;
+        case 5:
+            digits[n].segments.seg_b = 0;
+            digits[n].segments.seg_e = 0;
+            break;
+        case 6:
+            digits[n].segments.seg_b = 0;
+            break;
+        case 7:
+            digits[n].segments.seg_d = 0;
+            digits[n].segments.seg_e = 0;
+            digits[n].segments.seg_f = 0;
+            digits[n].segments.seg_g = 0;
+            break;
+        case 8:
+            break;
+        case 9:
+            digits[n].segments.seg_e = 0;
+            break;
+    }
+}
+
+static void setMinutes(uint8_t value) {
+    if (value > 100) return;
+    setDigit(0, value % 10);
+    setDigit(1, value / 10);
+}
+
+static void setHours(uint8_t value) {
+    if (value > 100) return;
+    setDigit(2, value % 10);
+    setDigit(3, value / 10);
+}
+
+// number can be a value between 1 and 4
+static void enableAlarmNumber(uint8_t number, bool enabled) {
+    if (enabled) digits[3].complete |= 1 << (6 + number);
+    else digits[3].complete &= ~(uint16_t)(1 << (6 + number));
+}
+
+static void enableDecimalPoint(bool enabled) {
+    digits[1].segments.seg_p = enabled;
+}
+
+static void enableFanIcon(bool enabled) {
+    digits[1].segments.seg_4 = enabled;
+}
+
+
+//===========================================================================
+// Display IC communication
+//===========================================================================
 
 static void pt6312SendCommand(uint8_t command, uint8_t options) {
     uint8_t txbuf[1] = {command | options};
@@ -78,7 +166,7 @@ static void pt6312SendCommand(uint8_t command, uint8_t options) {
 
 static void pt6312SendCompleteDigitData(void) {
     uint8_t txbuf[9] = {
-        PT6312_COMMAND_3,
+        PT6312_COMMAND_3 | PT6312_ADDRESS_DIGIT0,
         digits[0].bytes.low_byte,
         digits[0].bytes.high_byte,
         digits[1].bytes.low_byte,
@@ -105,7 +193,7 @@ static void pt6312SendCompleteDigitData(void) {
 // Driver configs and callbacks
 //===========================================================================
 
-const SPIConfig spicfg = {
+static const SPIConfig spicfg = {
   false,                    // no circular mode
   NULL,                     // end callback
   GPIOB,                    // chip select port
@@ -114,7 +202,7 @@ const SPIConfig spicfg = {
   0                         // SPI CR2 register
 };
 
-static PWMConfig pwmcfg = {
+static const PWMConfig pwmcfg = {
   100000,                           // 100 kHz PWM clock frequency
   1000,                             // Initial PWM period 1000 ms -> 100 Hz
   NULL,                             // no callback for timer update interrupt
@@ -146,10 +234,10 @@ THD_FUNCTION(displayThd, arg) {
     pwmEnableChannel(&PWMD8, 0, PWM_PERCENTAGE_TO_WIDTH(&PWMD8, 5000));
     pwmEnableChannel(&PWMD8, 1, PWM_PERCENTAGE_TO_WIDTH(&PWMD8, 5000));
 
-    digits[0].complete = 0x1;
-    digits[1].complete = 0x1;
-    digits[2].complete = 0x1;
-    digits[3].complete = 0x1;
+    digits[0].complete = 0;
+    digits[1].complete = 0;
+    digits[2].complete = 0;
+    digits[3].complete = 0;
 	
     // start SPI2 driver
     spiStart(&SPID2, &spicfg);
@@ -159,18 +247,26 @@ THD_FUNCTION(displayThd, arg) {
     pt6312SendCommand(PT6312_COMMAND_1, PT6312_MODE_4DIGITS);
     pt6312SendCommand(PT6312_COMMAND_4, PT6312_DISPLAY_ON | PT6312_PULSEWIDTH_MAX);
 
-    uint16_t current_value = 1;
+    uint8_t current_value = 0;
+    uint8_t alarm_number = 1;
+    bool foo = true;
 
     while(1) {
-        chThdSleepMilliseconds(85);
-        current_value <<= 1;
-        if (current_value == 0)
-            current_value = 1;
-        digits[0].complete = current_value;
-        digits[1].complete = current_value;
-        digits[2].complete = current_value;
-        digits[3].complete = current_value;  
+        chThdSleepMilliseconds(100);
+
+        setMinutes(current_value);
+        setHours(current_value);
+
+        enableAlarmNumber(alarm_number, true);
+        enableDecimalPoint(foo);
+        enableFanIcon(foo);
         
-        pt6312SendCompleteDigitData();      
+        pt6312SendCompleteDigitData();
+
+        enableAlarmNumber(alarm_number, false);
+
+        if (++current_value > 100) current_value = 0;
+        foo = !foo;
+        if (++alarm_number > 4) alarm_number = 1;
     }
 }
